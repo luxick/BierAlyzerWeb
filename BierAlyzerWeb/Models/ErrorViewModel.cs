bfc2b9dcc214f3ca1349f8105185140b0f3f42c0
Andre Beging <git@troogs.de>
2018-04-24 19:42:23 +0200

    Projektdateien hinzufügen.

---
+++ BierAlyzerWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BierAlyzerWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
